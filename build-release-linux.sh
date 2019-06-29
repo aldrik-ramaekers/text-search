681eb94001030c2c5cc86c47adca8162d675fe1c
Aldrik Ramaekers <aldrik.ramaekers@protonmail.com>
2019-06-29 16:11:26 +0200

    work

---
+++ build-release-linux.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm text-search_x64_linux.zip
+rm text-search_x64_linux_trial.zip
+
+cd src
+gcc -Wall -O3 -m64 -Wno-unused-label -rdynamic -Wno-unused-variable text_search.c -o ../release/text-search_x64_linux -lX11 -lGL -lGLU -lXrandr -lm -lpthread -ldl
+
+gcc -Wall -O3 -m64 -Wno-unused-label -DBUILD_TRIAL -rdynamic -Wno-unused-variable text_search.c -o ../release/text-search_x64_linux_trial -lX11 -lGL -lGLU -lXrandr -lm -lpthread -ldl
+
+if [ $? -ne 0 ]; then
+	cd ../
+	exit 1
+fi
+
+cd ../
+
+cp -r data/ release/
+
+cd release/
+
+zip text-search_x64_linux.zip text-search_x64_linux -r data
+zip text-search_x64_linux_trial.zip text-search_x64_linux_trial -r data
+
+rm text-search_x64_linux
+rm text-search_x64_linux_trial
+rm -rf data/
--- build-release-win.sh
+++ build-release-win.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-rm -rf release
-mkdir release
+rm text-search_x64_win.zip
+rm text-search_x64_win_trial.zip
+
 cd src
-gcc -Wall -O3 -m64 -Wno-unused-label -rdynamic -Wno-unused-variable text_search.c -o ../release/text-search_x64_linux -lX11 -lGL -lGLU -lXrandr -lm -lpthread -ldl
-
-gcc -Wall -O3 -m64 -Wno-unused-label -DBUILD_TRIAL -rdynamic -Wno-unused-variable text_search.c -o ../release/text-search_x64_linux_trial -lX11 -lGL -lGLU -lXrandr -lm -lpthread -ldl
-
 x86_64-w64-mingw32-gcc -Wall -O3 -m64 -Wno-unused-label -Wno-unused-variable text_search.c -o ../release/text-search_x64_win.exe -lopengl32 -lkernel32 -lglu32 -lgdi32 -lcomdlg32 -lole32 -lshlwapi
 
 x86_64-w64-mingw32-gcc -Wall -O3 -m64 -Wno-unused-label -DBUILD_TRIAL -Wno-unused-variable text_search.c -o ../release/text-search_x64_win_trial.exe -lopengl32 -lkernel32 -lglu32 -lgdi32 -lcomdlg32 -lole32 -lshlwapi
@@ -22,14 +19,9 @@ cp -r data/ release/
 
 cd release/
 
-zip text-search_x64_linux.zip text-search_x64_linux -r data
-zip text-search_x64_linux_trial.zip text-search_x64_linux_trial -r data
-
 zip text-search_x64_win.zip text-search_x64_win.exe -r data
 zip text-search_x64_win_trial.zip text-search_x64_win_trial.exe -r data
 
-rm text-search_x64_linux
-rm text-search_x64_linux_trial
 rm text-search_x64_win.exe
 rm text-search_x64_win_trial.exe
 rm -rf data/
