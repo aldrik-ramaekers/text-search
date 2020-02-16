479706e181157262c4eb86c6c855a3b4c2b9da10
Aldrik Ramaekers <aldrik.ramaekers@protonmail.com>
2020-02-16 12:02:00 +0100

    release build

--- release.sh
+++ release.sh
@@ -49,7 +49,7 @@ x86_64-w64-mingw32-ld -r -b binary -o ../bin/data.o \
 ../data/translations/en-English.mo \
 ../data/translations/nl-Dutch.mo \
 
-x86_64-w64-mingw32-gcc -Wall -m64 -O3 -Wno-unused-label -Wno-unused-variable text_search.c ../bin/data.o -o ../bin/text-search.exe ../misc/icon.res -lopengl32 -lkernel32 -lglu32 -lgdi32 -lcomdlg32 -lgdiplus -lole32 -lshlwapi
+x86_64-w64-mingw32-gcc -Wall -m64 -O3 -Wno-unused-label -Wno-unused-variable text_search.c ../bin/data.o -o ../bin/text-search.exe ../misc/icon.res -lopengl32 -lkernel32 -lglu32 -lgdi32 -lcomdlg32 -lgdiplus -lole32 -lshlwapi -lwininet
 
 rm -f ../bin/data.o
 cp --remove-destination ../bin/text-search.exe ../release/windows/text-search.exe
