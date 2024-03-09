4d3fea5faf60795bc90ceb51ccfa693dd6ddf5c2
Aldrik Ramaekers <aldrikboy@gmail.com>
2024-03-09 14:00:37 +0100

    release flag build

--- build_linux.sh
+++ build_linux.sh
@@ -1,11 +1,18 @@
 shopt -s extglob
 
 OUT_DIR="bin/debug"
+FLAGS="-g3 -Wall"
 
-mkdir -p "bin/debug"
+if [[ $* == *-release* ]]; then
+	OUT_DIR="bin/release"
+	FLAGS="-g3 -O3"
+fi
+
+mkdir -p $OUT_DIR
 
 ld -r -b binary -o bin/debug/data.obj LICENSE misc/logo_64.png imgui/LICENSE imfiledialog/LICENSE misc/search.png misc/folder.png
-g++ -m64 -std=c++17 -Wall -DUNICODE -o $OUT_DIR/text-search imgui/imgui*.cpp imgui/backends/imgui_impl_glfw.cpp src/*.cpp imfiledialog/*.cpp src/linux/*.cpp bin/debug/data.obj -Iimgui -Iimgui/backends -Isrc -Isrc/linux -pthread -ldl -lglfw -lGLU -lGL
+g++ -m64 -std=c++17 $FLAGS -DUNICODE -o $OUT_DIR/text-search imgui/imgui*.cpp imgui/backends/imgui_impl_glfw.cpp src/*.cpp imfiledialog/*.cpp src/linux/*.cpp bin/debug/data.obj -Iimgui -Iimgui/backends -Isrc -Isrc/linux -pthread -ldl -lglfw -lGLU -lGL
 
-
-./bin/debug/text-search
+if [[ $* == *-r* ]]; then
+	./$OUT_DIR/text-search
+fi
