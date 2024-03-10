22f40608ec8ba81577625bb65043cacbc51710eb
Aldrik Ramaekers <aldrikboy@gmail.com>
2024-03-10 11:54:43 +0100

    embed resources using header files close #2

--- build_win32.bat
+++ build_win32.bat
@@ -3,15 +3,15 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 @set OUT_EXE=text-search
 @set INCLUDES=/I..\.. /I..\..\backends
 @set SOURCES=imgui/imgui*.cpp src/*.cpp imfiledialog/*.cpp src/windows/*.cpp imgui/backends/imgui_impl_win32.cpp src/widgets/*.cpp
-@set LIBS=opengl32.lib Advapi32.lib Shell32.lib Ole32.lib User32.lib Pathcch.lib bin/debug/data.obj bin/debug/icon.res
+@set LIBS=opengl32.lib Advapi32.lib Shell32.lib Ole32.lib User32.lib Pathcch.lib bin/debug/icon.res
 @set FLAGS=
 windres misc/icon.rc -O coff -o bin/debug/icon.res
-ld -r -b binary -o bin/debug/data.obj LICENSE misc/logo_64.png imgui/LICENSE imfiledialog/LICENSE misc/search.png misc/folder.png
 
 if "%1"=="-release" (
 	@set OUT_DIR=bin\\release
 	@set FLAGS=/GL /O2 /DTS_RELEASE
 )
+
 mkdir %OUT_DIR%
 cl /std:c++17 /nologo %FLAGS% /W3 /Zi /MD /EHsc /Isrc/windows /Iimgui /Iimgui/backends /Isrc /utf-8 %INCLUDES% /D UNICODE /D _UNICODE %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fd%OUT_DIR%/vc140.pdb /Fo%OUT_DIR%/ /link %LIBS%
 if "%1"=="-r" call "bin/debug/text-search.exe"
--- src/definitions.cpp
+++ src/definitions.cpp
@@ -1,27 +1,25 @@
 #include "definitions.h"
 
-#ifdef __APPLE__
-#include "../misc/osx/LICENSE.h"
+#include "../misc/generated/LICENSE.h"
 unsigned char* _binary_LICENSE_start = LICENSE;
 unsigned char* _binary_LICENSE_end = _binary_LICENSE_start + LICENSE_len;
 
-#include "../misc/osx/imgui_LICENSE.h"
+#include "../misc/generated/imgui_LICENSE.h"
 unsigned char* _binary_imgui_LICENSE_start = imgui_LICENSE;
 unsigned char* _binary_imgui_LICENSE_end = imgui_LICENSE + imgui_LICENSE_len;
 
-#include "../misc/osx/imfiledialog_LICENSE.h"
+#include "../misc/generated/imfiledialog_LICENSE.h"
 unsigned char* _binary_imfiledialog_LICENSE_start = imfiledialog_LICENSE;
 unsigned char* _binary_imfiledialog_LICENSE_end = imfiledialog_LICENSE + imfiledialog_LICENSE_len;
 
-#include "../misc/osx/logo_64.h"
+#include "../misc/generated/logo_64.h"
 unsigned char* _binary_misc_logo_64_png_start = misc_logo_64_png;
 unsigned char* _binary_misc_logo_64_png_end = misc_logo_64_png + misc_logo_64_png_len;
 
-#include "../misc/osx/search.h"
+#include "../misc/generated/search.h"
 unsigned char* _binary_misc_search_png_start = misc_search_png;
 unsigned char* _binary_misc_search_png_end = misc_search_png + misc_search_png_len;
 
-#include "../misc/osx/folder.h"
+#include "../misc/generated/folder.h"
 unsigned char* _binary_misc_folder_png_start = misc_folder_png;
 unsigned char* _binary_misc_folder_png_end = misc_folder_png + misc_folder_png_len;
-#endif
