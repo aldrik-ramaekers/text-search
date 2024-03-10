e039ec4d059d49c6fafecbc9aec7536498baa8ec
Aldrik Ramaekers <aldrikboy@gmail.com>
2024-03-10 14:02:32 +0100

    osx app bundle

--- build_osx.sh
+++ build_osx.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 shopt -s extglob
 
 OUT_DIR="bin/debug"
@@ -13,6 +15,14 @@ mkdir -p $OUT_DIR
 # misc items are converted to header files, not embedded. (xxd -i LICENSE misc/osx/LICENSE.h)
 
 g++ -m64 -std=c++17 $FLAGS -DUNICODE -o $OUT_DIR/text-search imgui/imgui*.cpp imgui/backends/imgui_impl_glfw.cpp src/widgets/*.cpp src/*.cpp imfiledialog/*.cpp src/unix/*.cpp -Iimgui -Iimgui/backends -Isrc -Isrc/unix -pthread -ldl -lglfw -lGL
+
+if [[ $* == *-release* ]]; then
+	cp $OUT_DIR/text-search misc/text-search.app/Contents/MacOS/text-search
+	install_name_tool -change /usr/local/opt/glfw/lib/libglfw.3.dylib "@executable_path/../Library/libglfw.3.dylib" misc/text-search.app/Contents/MacOS/text-search
+	install_name_tool -change /usr/local/opt/mesa/lib/libGL.1.dylib "@executable_path/../Library/libGL.1.dylib" misc/text-search.app/Contents/MacOS/text-search
+	cp -R misc/text-search.app bin/release/text-search.app
+	rm misc/text-search.app/Contents/MacOS/text-search
+fi
 
 if [[ $* == *-r* ]]; then
 	./$OUT_DIR/text-search
